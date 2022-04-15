7a3be9b85c9663805df1700877fb6f83cb62e78e
MaxwellChrist <ribthulu@gmail.com>
2022-04-15 10:27:59 -0400

    stuck on button functionality

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class AppClass extends React.Component {
   
   state = {
-    turnCount: 0,
+    steps: 0,
     x: 2,
     y : 2,
     message: "",
@@ -12,19 +12,52 @@ export default class AppClass extends React.Component {
       ["", "", ""],
       ["", "B", ""],
       ["", "", ""],
-    ]
+    ],
+  }
+
+  getCoordinatesLeft(grid){
+    return grid[this.state.x - 1][this.state.y]
+  }
+
+  // const [x, y] = getCoordinates(matrix)
+  // console.log(`(${x}, ${y})`) // (1, 2)
+
+
+  goLeft = () => {
+    const updatedMatrix = [...this.state.matrix];
+    this.setState({
+      ...this.state,
+      x: this.state.x - 1,
+      steps: this.state.steps + 1,
+      updatedMatrix: this.getCoordinatesLeft(this.state.updatedMatrix),
+      matrix: updatedMatrix
+    })
+  }
+
+  goUp = () => {
+
+  }
+  goRight = () => {
+
+  }
+  goDown = () => {
+
+  }
+
+  getCoordinatesLeft(grid){
+    return grid[this.state.x - 1][this.state.y - 1]
   }
 
 
   render() {
 
-    console.log(this.state.matrix);
+    console.log(this.getCoordinatesLeft(this.state.matrix))
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
-          <h3 id="steps">You moved {this.state.turnCount} times</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           {this.state.matrix.flatMap(flatItem => flatItem).map((mapItem, index) => {
@@ -44,11 +77,11 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={() => this.goLeft()} id="left">LEFT</button>
+          <button onClick={() => this.goUp()} id="up">UP</button>
+          <button onClick={() => this.goRight()} id="right">RIGHT</button>
+          <button onClick={() => this.goDown()}id="down">DOWN</button>
+
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
