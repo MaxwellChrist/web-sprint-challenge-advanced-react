5f9de640a902805ba235faf2630c1cc6cbbbdc67
MaxwellChrist <ribthulu@gmail.com>
2022-04-15 09:09:08 -0400

    Think I added classname functionality properly and state within JSX  to render proper coordinates and turns in state

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -4,8 +4,8 @@ export default class AppClass extends React.Component {
   
   state = {
     turnCount: 0,
-    x: "",
-    y : "",
+    x: 2,
+    y : 2,
     message: "",
     email: "",
     matrix: [
@@ -17,17 +17,18 @@ export default class AppClass extends React.Component {
 
 
   render() {
-    console.log(this.state);
+
+    console.log(this.state.matrix);
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
+          <h3 id="steps">You moved {this.state.turnCount} times</h3>
         </div>
         <div id="grid">
           {this.state.matrix.flatMap(flatItem => flatItem).map((mapItem, index) => {
-            return <div key={index} className="square">{mapItem}</div>
+            return <div key={index} className={`square ${mapItem ? "active" : ""}`}>{mapItem}</div>
           })}
           {/* <div className="square"></div>
           <div className="square"></div>
